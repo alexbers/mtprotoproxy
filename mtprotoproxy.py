bd3d9731d730ba411d9d567ca220b76832ad8778
Alexander Bersenev <bay@hackerdom.ru>
2018-06-26 11:48:58 +0500

    if the handshake failed, just consume all the data

--- mtprotoproxy.py
+++ mtprotoproxy.py
@@ -487,6 +487,11 @@ async def handle_handshake(reader, writer):
         reader = CryptoWrappedStreamReader(reader, decryptor)
         writer = CryptoWrappedStreamWriter(writer, encryptor)
         return reader, writer, proto_tag, user, dc_idx, enc_key + enc_iv
+
+    while True:
+        # just consume all the data
+        await reader.read(READ_BUF_SIZE)
+
     return False
 
 
