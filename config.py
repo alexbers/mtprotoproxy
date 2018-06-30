76a43090540bb05504d43a61d411c9bcb9d73c28
Leonid Evdokimov <leon@darkk.net.ru>
2018-06-30 22:15:11 +0300

    Add Prometheus metrics exporter

--- config.py
+++ config.py
@@ -6,5 +6,9 @@ USERS = {
     "tg2": "0123456789abcdef0123456789abcdef"
 }
 
+# If you run two or more instances on same `PORT`, they should have _different_
+# prometheus ports to produce meaningful stats.
+# PROMETHEUS_PORT = 9500
+
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
