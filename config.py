66d9c03ff9b0c69d1c779a0b02c0b50ed970fd6e
Alexander Bersenev <bay@hackerdom.ru>
2019-03-10 23:02:27 +0500

    set secure mode on by default because most tg clients support this mode and many countries are able to detect proxies in non-secure mode

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ USERS = {
     "tg2": "0123456789abcdef0123456789abcdef"
 }
 
+# Makes the proxy harder to detect
+# Can be incompatible with very old clients
+SECURE_ONLY = True
+
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
-
-# Uncommenting this do make a proxy harder to detect
-# But it can be incompatible with old clients
-# SECURE_ONLY = True
