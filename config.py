5d7061a675479e46ba8080e03c8a885f8536db9c
KLDGodY <53929319+KLDGodY@users.noreply.github.com>
2020-03-15 20:31:16 +0800

    修改默认Fake TLS域名为 www.cloudflare.com

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.cloudflare.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
